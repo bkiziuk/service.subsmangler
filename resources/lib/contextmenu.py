8979456e10dcb65d3071e72f48ebaea923d021d0
bkiziuk <bkiziuk@users.noreply.github.com>
2021-02-07 16:42:29 +0100

    Fix context menu on Shows folders

--- resources/lib/contextmenu.py
+++ resources/lib/contextmenu.py
@@ -21,7 +21,10 @@ def main():
     # get the path of file clicked onto
     filepath = xbmc.getInfoLabel('ListItem.Path')
 
-    common.Log("Context menu invoked on: " + filepathname, xbmc.LOGINFO)
+    if xbmc.getCondVisibility('ListItem.IsFolder'):
+        common.Log("Context menu invoked on folder: " + filepath, xbmc.LOGINFO)
+    else:
+        common.Log("Context menu invoked on file: " + filepathname, xbmc.LOGINFO)
 
     # check if noautosubs file exists
     # do nothing if clicked item is not a real file
@@ -34,45 +37,45 @@ def main():
     if xbmc.getCondVisibility('ListItem.IsFolder'):
         # clicked item is a folder
         # check if folder contains noautosubs file
-        if (xbmcvfs.exists(os.path.join(filepathname, "noautosubs"))):
-            common.Log("'noautosubs' file exists: " + os.path.join(filepathname, "noautosubs   Opening YesNoDialog."),
+        if (xbmcvfs.exists(os.path.join(filepath, "noautosubs"))):
+            common.Log("Folder wide 'noautosubs' file exists: " + os.path.join(filepath, "noautosubs   Opening YesNoDialog."),
                        xbmc.LOGDEBUG)
             YesNoDialog = xbmcgui.Dialog().yesno("Subtitles Mangler", __addonlang__(
-                32107) + "\n" + filepathname + "\n" + __addonlang__(32104),
+                32107) + "\n" + filepath + "\n" + __addonlang__(32104),
                                                  nolabel=__addonlang__(32042),
                                                  yeslabel=__addonlang__(32043))
             # answering Yes deletes the file
             if YesNoDialog:
-                common.Log("Answer is Yes. Deleting file: " + os.path.join(filepathname, "noautosubs"), xbmc.LOGDEBUG)
+                common.Log("Answer is Yes. Deleting file: " + os.path.join(filepath, "noautosubs"), xbmc.LOGDEBUG)
                 # delete noautosubs file
-                common.DeleteFile(os.path.join(filepathname, "noautosubs"))
+                common.DeleteFile(os.path.join(filepath, "noautosubs"))
             else:
                 common.Log("Answer is No. Doing nothing.", xbmc.LOGDEBUG)
         else:
-            common.Log("'noautosubs' file does not exist in: " + filepathname + "   Opening YesNoDialog.",
+            common.Log("Folder wide 'noautosubs' file does not exist in: " + filepath + "   Opening YesNoDialog.",
                        xbmc.LOGDEBUG)
             YesNoDialog = xbmcgui.Dialog().yesno("Subtitles Mangler", __addonlang__(
-                32108) + "\n" + filepathname + "\n" + __addonlang__(32106),
+                32108) + "\n" + filepath + "\n" + __addonlang__(32106),
                                                  nolabel=__addonlang__(32042),
                                                  yeslabel=__addonlang__(32043))
             # answering Yes creates the file
             if YesNoDialog:
-                common.Log("Answer is Yes. Creating file: " + os.path.join(filepathname, "noautosubs"), xbmc.LOGDEBUG)
-                # create .noautosubs file
-                common.CreateNoAutoSubsFile(os.path.join(filepathname, "noautosubs"))
+                common.Log("Answer is Yes. Creating file: " + os.path.join(filepath, "noautosubs"), xbmc.LOGDEBUG)
+                # create noautosubs file
+                common.CreateNoAutoSubsFile(os.path.join(filepath, "noautosubs"))
             else:
                 common.Log("Answer is No. Doing nothing.", xbmc.LOGDEBUG)
     else:
         # clicked item is a file
-        # check if folder contains .noautosubs file
+        # check if folder contains noautosubs file
         if (xbmcvfs.exists(os.path.join(filepath, "noautosubs"))):
-            common.Log("'noautosubs' file exists: " + os.path.join(filepath, "noautosubs   Opening Ok dialog."),
+            common.Log("Folder wide 'noautosubs' file exists: " + os.path.join(filepath, "noautosubs   Opening Ok dialog."),
                        xbmc.LOGDEBUG)
             xbmcgui.Dialog().ok("Subtitles Mangler",
                                 __addonlang__(32101) + "\n" + filepath + "\n" + __addonlang__(32102))
         else:
-            common.Log("'noautosubs' file does not exist in: " + filepath, xbmc.LOGDEBUG)
-            # check if .noautosubs extension exists
+            common.Log("Folder wide 'noautosubs' file does not exist in: " + filepath, xbmc.LOGDEBUG)
+            # check if .noautosubs extension flag is set on clicked file
             filebase, _fileext = os.path.splitext(filepathname)
             if (xbmcvfs.exists(filebase + ".noautosubs")):
                 # extension flag is set for this file
